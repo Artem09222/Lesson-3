4e2c99d49bce31d04260249a7b90d466eb9b58d7
Artem09222 <107929764+Artem09222@users.noreply.github.com>
2023-02-21 12:25:53 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// Exersice 1
+
+console.log("Exersice 1");
+for (let i = 3; i <= 20; i++) {
+   console.log(i);
+}
+
+//Exersice 2
+
+console.log("Exersice 2");
+for (let f = 10; f <= 20; f++) {
+    console.log(f);
+ }
+ 
+
+//Exersice 3
+
+console.log("Exersice 3");
+
+let n1 = prompt();
+let n2 = prompt();
+let number1 = Number(n1);
+let number2 = Number(n2);
+for (let l = number1; l <= number2; l++) {
+    console.log(l);
+}
+
